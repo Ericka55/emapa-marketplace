6e820ada93d07d1d69b9a03c02e56c31ed6eaeb2
Ericka55 <erickamorat55@gmail.com>
2026-06-02 13:44:48 -0400

    add estadoSolicitud seed data2

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,15 +4,33 @@ const prisma = new PrismaClient();
 
 async function main() {
 
-  // ESTADOS DE SOLICITUD
-  await prisma.estadoSolicitud.createMany({
-    data: [
-      { nombre: "PENDIENTE" },
-      { nombre: "APROBADO" },
-      { nombre: "RECHAZADO" }
-    ],
-    skipDuplicates: true
-  });
+  await prisma.categoria.createMany({
+  data: [
+    { nombre: "Granos", descripcion: "Arroz, trigo, maíz" },
+    { nombre: "Frutas", descripcion: "Frutas frescas" },
+    { nombre: "Verduras", descripcion: "Hortalizas y verduras" },
+    { nombre: "Lácteos", descripcion: "Leche, queso, yogurt" }
+  ],
+  skipDuplicates: true
+});
+await prisma.estadoIncidencia.createMany({
+  data: [
+    { nombre: "PENDIENTE" },
+    { nombre: "EN_REVISION" },
+    { nombre: "RESUELTO" },
+    { nombre: "RECHAZADO" }
+  ],
+  skipDuplicates: true
+});
+await prisma.metodoPago.createMany({
+  data: [
+    { nombre: "EFECTIVO" },
+    { nombre: "QR" },
+    { nombre: "TARJETA" },
+    { nombre: "TRANSFERENCIA" }
+  ],
+  skipDuplicates: true
+});
 
   console.log("Estados de solicitud creados");
 
